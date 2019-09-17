0b3f26cc90cd9b4241fc33a0693301b83f15086a
Jaden <jadenzerbe@gmail.com>
2019-09-17 18:43:24 -0400

    Added test graph componenet to investments page

--- frontend/src/components/Investments/Investments.js
+++ frontend/src/components/Investments/Investments.js
@@ -1,10 +1,32 @@
 import React from 'react';
-
+import CanvasJSReact from '../../assets/canvasjs.react';
+var CanvasJS = CanvasJSReact.CanvasJS;
+var CanvasJSChart = CanvasJSReact.CanvasJSChart;
 class Investments extends React.Component {
   render () {
+      const options = {
+      title: {
+        text: "Basic Column Chart in React"
+      },
+      data: [{				
+                type: "column",
+                dataPoints: [
+                    { label: "Apple",  y: 10  },
+                    { label: "Orange", y: 15  },
+                    { label: "Banana", y: 25  },
+                    { label: "Mango",  y: 30  },
+                    { label: "Grape",  y: 28  }
+                ]
+       }]
+   }
     return (
       <div className="BigDivArea">
         <h3>Investments!</h3>
+        <div>
+        <CanvasJSChart options = {options}
+            /* onRef = {ref => this.chart = ref} */
+        />
+      </div>
       </div>
     );
   }
