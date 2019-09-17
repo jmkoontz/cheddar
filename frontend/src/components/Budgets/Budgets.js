3ef7fe4537fe4bc2a943ccbc8a01d167afbdf5c5
Samuel Ingram <samingram22@gmail.com>
2019-09-17 11:44:14 -0400

    Initialize Basic Front End

---
+++ frontend/src/components/Budgets/Budgets.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Budgets extends React.Component {
+  render () {
+    return (
+      <div className="BigDivArea">
+        <h3>Budgets!</h3>
+      </div>
+    );
+  }
+}
+
+export default Budgets;
---
+++ frontend/src/components/Saving/Saving.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Saving extends React.Component {
+  render () {
+    return (
+      <div className="BigDivArea">
+        <h3>Saving!</h3>
+      </div>
+    );
+  }
+}
+
+export default Saving;
