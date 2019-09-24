21bfa97e1953ac0ae85cb1de39f4237739a5679b
Jake Koontz <jmkoontz@purdue.edu>
2019-09-23 23:35:51 -0400

    Added more backend budget support

--- backend/controllers/budgetController.js
+++ backend/controllers/budgetController.js
@@ -1,7 +1,8 @@
 import bodyParser from 'body-parser';
 
 import {parseError, buildResponse} from '../utilities/controllerFunctions';
-import {getAllBudgets, createBudget} from '../models/budgetDAO';
+import {getAllBudgets, getBudgetNames, getBudgetCategoryNames, createBudget,
+    addBudgetCategory} from '../models/budgetDAO';
 
 export default (app) => {
   app.post('/Cheddar/Budgets/:uid', async (req, res) => {
@@ -23,11 +24,47 @@ export default (app) => {
     buildResponse(res, data);
   });
 
+  // add budget category
+  app.post('/Cheddar/Budgets/Budget/:uid/:name', async (req, res) => {
+    let data;
+    try {
+      data = await addBudgetCategory(req.params.uid, req.params.name, budget);
+    } catch (err) {
+      data = {error: parseError(err)};
+    }
+
+    buildResponse(res, data);
+  });
+
   // get all budgets
   app.get('/Cheddar/Budgets/:uid', async (req, res) => {
     let data;
     try {
       data = await getAllBudgets(req.params.uid);
+    } catch (err) {
+      data = {error: parseError(err)};
+    }
+
+    buildResponse(res, data);
+  });
+
+  // get all budget names (testing)
+  app.get('/Cheddar/Budgets/Names/:uid', async (req, res) => {
+    let data;
+    try {
+      data = await getBudgetNames(req.params.uid);
+    } catch (err) {
+      data = {error: parseError(err)};
+    }
+
+    buildResponse(res, data);
+  });
+
+  // get all budget category names (testing)
+  app.get('/Cheddar/Budgets/Budget/Names/:uid/:name', async (req, res) => {
+    let data;
+    try {
+      data = await getBudgetCategoryNames(req.params.uid, req.params.name);
     } catch (err) {
       data = {error: parseError(err)};
     }
--- backend/models/budgetDAO.js
+++ backend/models/budgetDAO.js
@@ -1,7 +1,12 @@
 import {userModel, budgetModel} from '../utilities/mongooseModels';
 
 export function getAllBudgets(uid) {
-  return userModel.findOne({_id: uid})
+  const returnClause = {
+    '_id': 0, // exclude _id
+    'budgets': 1
+  };
+
+  return userModel.findOne({_id: uid}, returnClause)
     .then((user) => {
       if (user)
         return Promise.resolve(user);
@@ -13,7 +18,54 @@ export function getAllBudgets(uid) {
     });
 }
 
-export function createBudget(uid, budget) {
+export function getBudgetNames(uid) {
+  const returnClause = {
+    '_id': 0, // exclude _id
+    'budgets.name': 1
+  };
+
+  return userModel.findOne({_id: uid}, returnClause)
+    .then((user) => {
+      if (user && user.budgets) {
+        let namesList = [];
+        for (let i in user.budgets)
+          namesList.push(user.budgets[i].name);
+
+        return Promise.resolve(namesList);
+      } else {
+        return Promise.reject('UserError: User not found');
+      }
+    })
+    .catch((err) => {
+      return Promise.reject(err);
+    });
+}
+
+export function getBudgetCategoryNames(uid, budgetName) {
+  const returnClause = {
+    '_id': 0, // exclude _id
+    'budgets': {'$elemMatch': {'name': budgetName}},
+    'budgets.budgetCategories.name': 1
+  };
+
+  return userModel.findOne({_id: uid}, returnClause)
+    .then((user) => {
+      if (user && user.budgets) {
+        let namesList = [];
+        for (let i in user.budgets[0].budgetCategories)
+          namesList.push(user.budgets[0].budgetCategories[i].name);
+
+        return Promise.resolve(namesList);
+      } else {
+        return Promise.reject('UserError: User not found');
+      }
+    })
+    .catch((err) => {
+      return Promise.reject(err);
+    });
+}
+
+export async function createBudget(uid, budget) {
   for (let i in budget) {
     if (budget.hasOwnProperty(i)) {
       if (budget[i] === undefined)
@@ -23,15 +75,60 @@ export function createBudget(uid, budget) {
     }
   }
 
-  // TODO Fix
+  // get names of budgets
+  let budgetNames = [];
+  try {
+    budgetNames = await getBudgetNames(uid);
+  } catch (error) {
+    return Promise.reject(error);
+  }
+
+  // protect against duplicate budget names
+  if (budgetNames.includes(budget.name))
+    return Promise.reject('UserError: Budget with name \"' + budget.name + '\" already exists');
+
   return userModel.findOneAndUpdate(
     {_id: uid},
-    {'$addToSet': {'budgets': budget}},
-    {rawResult: true})
+    {'$addToSet': {'budgets': budget}})
     .then((originalUser) => {
-      console.log(originalUser)
       if (originalUser == null)
-        return Promise.reject('UserError: User does not exist');
+        return Promise.reject('UserError: User not found');
+
+      return Promise.resolve(originalUser);
+    })
+    .catch((err) => {
+      return Promise.reject(err);
+    });
+}
+
+export async function addBudgetCategory(uid, budgetName, category) {
+  for (let i in category) {
+    if (category.hasOwnProperty(i)) {
+      if (category[i] === undefined)
+        return Promise.reject('UserError: One or more fields are missing');
+      else if (category[i] === '')
+        return Promise.reject('UserError: Each field must have information');
+    }
+  }
+
+  // get names of budgets
+  let categoryNames = [];
+  try {
+    categoryNames = await getBudgetCategoryNames(uid, budgetName);
+  } catch (error) {
+    return Promise.reject(error);
+  }
+
+  // protect against duplicate budget names
+  if (categoryNames.includes(category.name))
+    return Promise.reject('UserError: Budget category with name \"' + category.name + '\" already exists');
+
+  return userModel.findOneAndUpdate(
+    {_id: uid},
+    {'$addToSet': {'budgets': budget}})
+    .then((originalUser) => {
+      if (originalUser == null)
+        return Promise.reject('UserError: User not found');
 
       return Promise.resolve(originalUser);
     })
