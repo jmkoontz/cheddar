0551424f470590977e9bf5234288e05176728d74
Jeremy <jeremyputput@gmail.com>
2019-09-30 22:23:46 -0400

    Goofed and forgot to change a variable name

--- frontend/src/components/Budgets/StudentLoan.js
+++ frontend/src/components/Budgets/StudentLoan.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Form, FormGroup, Label, Input, Alert } from 'reactstrap';
-import { Row, Col, Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+import { Row, Col } from 'reactstrap';
 import axios from 'axios';
 import '../../css/Budgets.css';
 import FormBody from "./FormBody";
@@ -10,13 +10,13 @@ function StudentLoan(props) {
   const [dataEnter, setDataEnter] = useState(false); // Boolean to determine if user entered all their data
   const [amount, setAmount] = useState(); // Variable to hold the amount
   const [timeframe, setTimeframe] = useState(); // Variable to hold the timeframe
-  
+
   /**
    * Function to calculate how much debt to pay a month
    */
   const calculatePayment = () => {
     let monthlyPayment = amount / timeframe;
-    
+
     // Make a tmp object to store the calculated amount
     let tmpObj = {
       name: "Debt",
@@ -30,23 +30,23 @@ function StudentLoan(props) {
   /**
    * Helper method to handle user changes to amount
    */
-	const handleAmountChange = (event) => {
-		setAmount(parseFloat(event.target.value));
+  const handleAmountChange = (event) => {
+    setAmount(parseFloat(event.target.value));
   }
-  
+
   /**
    * Helper method to handle user changes to timeframe
    */
-	const handleTimeChange = (event) => {
-		setTimeframe(parseFloat(event.target.value));
-	}
+  const handleTimeChange = (event) => {
+    setTimeframe(parseFloat(event.target.value));
+  }
 
   useEffect(
-		() => {
-			
-		},
-		[props]
-	);
+    () => {
+
+    },
+    [props]
+  );
 
   return (
     <div>
@@ -54,13 +54,17 @@ function StudentLoan(props) {
         ?
         <div>
           <Row className="addSpace">
-            <Label for="loan">Loan Amount</Label>
-            <Input onChange={handleAmountChange} name="loan" id="loan" placeholder="10000" />
-            <Label for="timeframe">Timeframe of Loan</Label>
-            <Input onChange={handleTimeChange} name="timeframe" id="timeframe" placeholder="Enter number of months" />
+            <Col>
+              <Label for="loan">Loan Amount</Label>
+              <Input onChange={handleAmountChange} name="loan" id="loan" placeholder="10000" />
+              <Label for="timeframe">Timeframe of Loan</Label>
+              <Input onChange={handleTimeChange} name="timeframe" id="timeframe" placeholder="Enter number of months" />
+            </Col>
           </Row>
           <Row>
-            <Button color="success" onClick={calculatePayment}>Enter Data</Button>
+            <Col>
+              <Button color="success" onClick={calculatePayment}>Enter Data</Button>
+            </Col>
           </Row>
         </div>
         :
