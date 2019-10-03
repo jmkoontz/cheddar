0a9d07dda7dea3007fb74ae2306863d2cc073945
Eric <31486565+eric-thiem@users.noreply.github.com>
2019-10-02 17:46:09 -0700

    added account settings ui

--- frontend/src/components/Accounts/AccountSettings.js
+++ frontend/src/components/Accounts/AccountSettings.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Input, Button, Alert, Row, Col} from 'reactstrap';
+import { Input, Form, Button, Alert, Row, Col} from 'reactstrap';
+import { fireauth } from '../../firebase.js';
 import './SignIn.css'
 
 class AccountSettings extends Component {
@@ -18,16 +19,57 @@ class AccountSettings extends Component {
 
   };
 
+  updatePassword = () => {
+
+  };
+
+  getEmail = () => {
+    return fireauth.currentUser.email;
+  };
 
   render(){
     return(
 
       <div className="BigDivArea">
         <h3>Edit Account Settings</h3>
-        <Row>
-          <Col md='3'/>
-          <Col md='2'>
+        <hr/>
 
+        <Row>
+          <Col md='4'/>
+          <Col md='4'>
+            <Input type='text' readOnly bsSize='lg' placeholder={this.getEmail()}/>
+          </Col>
+        </Row>
+
+        <hr/>
+        <Form onSubmit={this.updatePassword}>
+          <Row>
+            <Col md='4'/>
+            <Col md='2'>
+              <Input type='text' id='newpassword' bsSize='lg' placeholder='New Password' style={{border: '1px solid #4682B4'}}/>
+            </Col>
+          </Row>
+
+          <div style={{height: '1em'}}/>
+
+          <Row>
+            <Col md='4'/>
+            <Col md='2'>
+              <Input type='text' id='confirmpassword' bsSize='lg' placeholder='Confirm Password' style={{border: '1px solid #4682B4'}}/>
+            </Col>
+            <Col md='1'>
+              <Button className='signInButton' size='lg'> Update Password </Button>
+            </Col>
+          </Row>
+        </Form>
+
+        <hr/>
+        <div style={{height: '2em'}}/>
+
+        <Row>
+          <Col md='5'/>
+          <Col md='2'>
+            <Button color='danger' onClick={this.deleteAccount}> Delete My Account </Button>
           </Col>
         </Row>
       </div>
