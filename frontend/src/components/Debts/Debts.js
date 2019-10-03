2f44f2ea7a75e8f46aed3a3462e166ad4f8652cb
kevm124 <kmurciab@purdue.edu>
2019-10-03 15:24:10 -0400

    Debts now render dynamically and adding new debt works

--- frontend/src/components/Debts/Debts.js
+++ frontend/src/components/Debts/Debts.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import { Component } from 'react';
+import { Button } from 'reactstrap';
 import { withRouter } from "react-router-dom";
 import History from "../../history";
 import Modal from 'react-bootstrap/Modal'
+import axios from 'axios';
+
+const DebtModel = ({category, nickname, initial, currBalance, interestRate}) => (
+  <div>
+    {nickname && <h3>{nickname}</h3>}
+    <h2>{category}</h2>
+    <p>Current Balance: ${currBalance}<br/>Interest Rate: {interestRate}%</p>
+  </div>
+)
 
 class Debts extends React.Component {
   constructor(props){
     super(props);
-    this.state = {show: false, setShow: false, category: '', nickname: '', initial: '', currBalance: '', interestRate: ''}
+    this.state = { userID: sessionStorage.getItem('user'), show: false, debtList: [], category: '', nickname: '', initial: '', currBalance: '', interestRate: ''}
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -29,17 +39,50 @@ class Debts extends React.Component {
   }
 
   handleSubmit(event){
-    alert('A new goal \'' + this.state.title + '\' of $' + this.state.goalAmount + ' was submitted in ' + this.state.category + '\nYou plan to save $' + this.state.monthlyCont + ' a month until ' + this.state.month + ' ' + this.state.year);
-    event.preventDefault();
-    this.setState({show: false})
+    //alert('A new goal \'' + this.state.title + '\' of $' + this.state.goalAmount + ' was submitted in ' + this.state.category + '\nYou plan to save $' + this.state.monthlyCont + ' a month until ' + this.state.month + ' ' + this.state.year);
+    axios.post(`http://localhost:8080/Cheddar/Debts/${this.state.userID}`,
+      {
+        category: this.state.category,
+        nickname: this.state.nickname,
+        initial: this.state.initial,
+        currBalance: this.state.currBalance,
+        interestRate: this.state.interestRate
+      })
+      .then((response) => {
+        console.log(response);
+        event.preventDefault();
+        this.getDebts();
+        this.setState({show: false})
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  getDebts = () => {
+    axios.get(`http://localhost:8080/Cheddar/Debts/${this.state.userID}/`)
+      .then((response) => {
+        this.setState({debtList: response.data})
+      })
+      .catch((error) => {
+        console.error("Error getting Debts\n" + error);
+      });
+  }
+
+  componentDidMount(){
+    this.getDebts();
   }
 
   render () {
+    const debts = this.state.debtList;
     return (
       <div className="BigDivArea">
         <h3>Debts!</h3>
+        {(debts.length > 0 && debts[0])
+          ? debts.map(plan => <DebtModel {...plan} />)
+          : <p>Keep track of all the debts you may have here. Start by adding one below</p>}
          <span className="input-group-btn">
-              <button onClick={this.handleClick} type="button">+</button>
+              <Button outline color="secondary" onClick={this.handleClick} type="button">Add +</Button>
         </span>
 
         <Modal show={this.state.show} onHide={this.handleClose} size="lg" aria-labelledby="contained-modal-title-vcenter" centered>
@@ -68,8 +111,8 @@ class Debts extends React.Component {
               </label>
               <br/>
               <label>
-                <b>Monthly Contribution</b><br/>
-                <input name="interestRate" type="number" value={this.state.interestRate} onChange={this.handleChange} />
+                <b>Interest Rate</b><br/>
+                <input name="interestRate" type="number" step="0.01" value={this.state.interestRate} onChange={this.handleChange} />
               %</label><br/>
               <b>Nickname</b> (<i>optional</i>)<br/>
                 <input name="title" type="text" value={this.state.title} onChange={this.handleChange} />
