2bedf72d1b17dedbbea55a77f991b0af7a3c42b3
kevm124 <kmurciab@purdue.edu>
2019-10-03 12:36:31 -0400

    Added basic debt frontend and backend functionality

--- backend/utilities/mongooseModels.js
+++ backend/utilities/mongooseModels.js
@@ -1,9 +1,10 @@
 import mongoose from 'mongoose';
 
-import {userSchema, budgetSchema, budgetCategorySchema, transactionSchema, savingsSchema} from './mongooseSchemas';
+import {userSchema, budgetSchema, budgetCategorySchema, transactionSchema, savingsSchema, debtSchema} from './mongooseSchemas';
 
 export const userModel = mongoose.model('User', userSchema);
 export const budgetModel = mongoose.model('Budget', budgetSchema);
 export const budgetCategoryModel = mongoose.model('BudgetCategory', budgetCategorySchema);
 export const transactionModel = mongoose.model('Transaction', transactionSchema);
-export const savingsModel = mongoose.model('Savings', savingsSchema); 
+export const savingsModel = mongoose.model('Savings', savingsSchema);
+export const debtModel = mongoose.model('Debts', debtSchema);
