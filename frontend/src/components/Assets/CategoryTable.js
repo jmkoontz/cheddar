41ddb047b98e2ef791ae7fccd0e6b25e38faec10
Eric <31486565+eric-thiem@users.noreply.github.com>
2019-10-04 00:49:39 -0700

    table working

--- frontend/src/components/Assets/CategoryTable.js
+++ frontend/src/components/Assets/CategoryTable.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Modal, ModalHeader, ModalBody, Input, Form, Button, Alert, Row, Col, Table} from 'reactstrap';
+import '../Accounts/SignIn.css'
 
 class CategoryTable extends Component {
 
@@ -8,44 +9,73 @@ class CategoryTable extends Component {
 
     this.state = {
       category_name: props.category_name,
-    }
+      assets: []
+    };
+
+    let asset1 = {
+      asset_name: 'Car',
+      asset_value: 1000,
+    };
+    let asset2 = {
+      asset_name: 'Car',
+      asset_value: 1000,
+    };
+
+    this.addNewAsset(asset1);
+    this.addNewAsset(asset2);
   }
+
+  addNewAsset = (asset) => {
+    this.state.assets.push(asset);
+  };
+
+  renderTable = () => {
+    let i = 0;
+    let j = 1;
+    let table = [];
+    let self = this;
+    this.state.assets.forEach(function() {
+      table.push(
+        <tr key={i}>
+          <th scope='row'>{j}</th>
+          <td>{self.state.assets[i].asset_name}</td>
+          <td>{self.state.assets[i].asset_value}</td>
+          <td>{self.getRemoveButton(i)}</td>
+        </tr>
+      );
+      i++;
+      j++;
+    });
+    return table;
+  };
+
+  getRemoveButton(i){
+    return (
+      <Button color='danger'>-</Button>
+    );
+  };
 
   render (){
     return (
       <div>
         <Row>
           <h3>Phyical Assets</h3>
+          <Col md='8'/>
+          <Button className='assetButton'>Add New Asset</Button>
         </Row>
+        <div style={{height: '1em'}}/>
 
         <Table dark>
           <thead>
           <tr>
             <th>#</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Username</th>
+            <th>Asset</th>
+            <th>Value</th>
+            <th>Remove Asset</th>
           </tr>
           </thead>
           <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td>Larry</td>
-            <td>the Bird</td>
-            <td>@twitter</td>
-          </tr>
+          {this.renderTable()}
           </tbody>
         </Table>
       </div>
