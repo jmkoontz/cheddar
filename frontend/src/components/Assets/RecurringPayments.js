c864eeb43035c4af2b577a3ca9531507c0b0c553
Eric <31486565+eric-thiem@users.noreply.github.com>
2019-11-01 08:04:14 -0700

    edit recurring payments

--- frontend/src/components/Assets/RecurringPayments.js
+++ frontend/src/components/Assets/RecurringPayments.js
@@ -24,6 +24,16 @@ class RecurringPayments extends Component {
       modal: false,
       dropdown: false,
       timePeriod: "Monthly",
+      editTimePeriod: "Monthly",
+
+      editModal: false,
+      editPayment: {
+        payment_name: null,
+        amount: null,
+        date: null,
+        timePeriod: null,
+      },
+      editIndex: null,
 
       show1: {
         payment_name: "Rent",
@@ -71,6 +81,12 @@ class RecurringPayments extends Component {
     });
   };
 
+  toggleEditModal = () => {
+    this.setState({
+      editModal: !this.state.editModal,
+    });
+  };
+
   goToAssets = () => {
     History.push("/assets");
   };
@@ -84,6 +100,12 @@ class RecurringPayments extends Component {
   selectTimePeriod = (ev) => {
     this.setState({
       timePeriod: ev.target.innerText,
+    });
+  };
+
+  selectEditTimePeriod = (ev) => {
+    this.setState({
+      editTimePeriod: ev.target.innerText,
     });
   };
 
@@ -116,6 +138,50 @@ class RecurringPayments extends Component {
       payments: payments,
     });
     ref.forceUpdate();
+  };
+
+  editItem = (ref, i) => {
+    ref.setState({
+      editModal: true,
+      editPayment: this.state.payments[i],
+      editIndex: i,
+    })
+  };
+
+  confirmEdit = (ev) => {
+    ev.preventDefault();
+    let payment_name = ev.target.payment_name.value;
+    let amount = ev.target.amount.value;
+    let timePeriod = this.state.timePeriod;
+    let date = ev.target.date.value;
+
+    let payment = {
+      payment_name: payment_name,
+      amount: amount,
+      timePeriod: timePeriod,
+      date: date
+    };
+
+    let payments = this.state.payments;
+    let oldPayment = payments[this.state.editIndex];
+
+    if(payment.payment_name === ''){
+      payment.payment_name = oldPayment.payment_name;
+    }
+
+    if(payment.amount === ''){
+      payment.amount = oldPayment.amount;
+    }
+
+    if(payment.date === ''){
+      payment.date = oldPayment.date;
+    }
+
+    payments[this.state.editIndex] = payment;
+    this.setState({
+      payments: payments,
+      editModal: false,
+    });
   };
 
   render(){
@@ -174,6 +240,53 @@ class RecurringPayments extends Component {
           </ModalBody>
         </Modal>
 
+        <Modal isOpen={this.state.editModal} toggle={this.toggleEditModal}>
+          <ModalHeader toggle={this.toggleEditModal}> Edit A Payment </ModalHeader>
+          <ModalBody>
+            <Form onSubmit={this.confirmEdit}>
+              <Row>
+                <Col md='7'>
+                  <Input type='text' id='payment_name' placeholder={this.state.editPayment.payment_name}/>
+                </Col>
+                <Col md='5'>
+                  <InputGroup>
+                    <InputGroupAddon addonType='prepend'>$</InputGroupAddon>
+                    <Input type='number' id='amount' placeholder={this.state.editPayment.amount}/>
+                  </InputGroup>
+                </Col>
+              </Row>
+              <div style={{height: '1em'}}/>
+              <Row>
+                <Col md='7'>
+                  <Dropdown size='md' isOpen={this.state.dropdown} toggle={this.toggle}>
+                    <DropdownToggle caret>{this.state.editTimePeriod}</DropdownToggle>
+                    <DropdownMenu>
+                      <DropdownItem onClick={this.selectEditTimePeriod}>Daily</DropdownItem>
+                      <DropdownItem onClick={this.selectEditTimePeriod}>Monthly</DropdownItem>
+                      <DropdownItem onClick={this.selectEditTimePeriod}>Yearly</DropdownItem>
+                    </DropdownMenu>
+                  </Dropdown>
+                </Col>
+                <Col md='5'>
+                  <FormGroup>
+                    <Input
+                      type="date"
+                      name="date"
+                      id="date"
+                      placeholder={this.state.editPayment.date}
+                    />
+                  </FormGroup>
+                </Col>
+              </Row>
+              <div style={{height: '1em'}}/>
+              <Row>
+                <Col md='3'/>
+                <Button className='signInButton' size='sm'>Confirm Edit</Button>
+              </Row>
+            </Form>
+          </ModalBody>
+        </Modal>
+
         <div className='right'>
           <Row>
             <Button size='sm' onClick={this.goToAssets}>Go To Assets</Button>
@@ -205,7 +318,7 @@ class RecurringPayments extends Component {
                 <td>${this.state.payments[i].amount}</td>
                 <td>{this.state.payments[i].date}</td>
                 <td>{this.state.payments[i].timePeriod}</td>
-                <td><Button color='info'>O</Button></td>
+                <td><Button color='info' onClick={() => this.editItem(this, i)}>O</Button></td>
                 <td><Button color='danger' onClick={() => this.removeItem(this, i)}>-</Button></td>
               </tr>
             );
