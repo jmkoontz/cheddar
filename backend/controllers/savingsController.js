0d904439e203f97ceb3c68c0d933f664b3bf76d1
kevm124 <kmurciab@purdue.edu>
2019-12-02 19:30:20 -0500

    Added backend to favorite savings and debts

--- backend/controllers/savingsController.js
+++ backend/controllers/savingsController.js
@@ -1,7 +1,7 @@
 import bodyParser from 'body-parser';
 
 import {parseError, buildResponse} from '../utilities/controllerFunctions';
-import {getAllSavings, getSavingsTitles, createSavings, editSavings, deleteSavings, getOneSavings} from '../models/savingsDAO';
+import {getAllSavings, getSavingsTitles, createSavings, editSavings, deleteSavings, getOneSavings, favSavings, unfavSavings} from '../models/savingsDAO';
 
 export default (app) => {
   app.get('/Cheddar/Savings/test', async (req, res) => {
@@ -104,6 +104,28 @@ export default (app) => {
       data = {error: parseError(err)};
     }
 
+    buildResponse(res, data);
+  });
+
+  // favorite a savings plans
+  app.put('/Cheddar/Savings/Favorite/:uid/:savingsId', async (req, res) => {
+    let data;
+    try{
+      data = await favSavings(req.params.uid, req.params.savingsId);
+    } catch (err) {
+      data = {error: parseError(err)};
+    }
+    buildResponse(res, data);
+  });
+
+  // unfavorite a savings plans
+  app.put('/Cheddar/Savings/Unfavorite/:uid/:savingsId', async (req, res) => {
+    let data;
+    try{
+      data = await unfavSavings(req.params.uid, req.params.savingsId);
+    } catch (err) {
+      data = {error: parseError(err)};
+    }
     buildResponse(res, data);
   });
 };
--- backend/models/savingsDAO.js
+++ backend/models/savingsDAO.js
@@ -144,3 +144,51 @@ export function deleteSavings(uid, savingsId) {
       return Promise.reject(err);
     });
 }
+
+export async function favSavings(uid, savingsId){
+  var savings;
+  try{
+    savings = await getAllSavings(uid);
+  } catch (err){
+    return Promise.reject(err);
+  }
+
+  for(let i in savings){
+    if(savings[i]._id === savingsId){
+      savings[i].favorite = true;
+    }
+    else if(savings[i].favorite === true){
+      savings[x].favorite = false;
+    }
+  }
+
+  return userModel.findOneAndUpdate(
+    {'_id': uid},
+    {'$set': {'savings': savings}},
+    {'new': true})
+    .then((updatedUser) => {
+      if (updatedUser == null)
+        return Promise.reject('UserError: User or savings not found');
+
+      return Promise.resolve(updatedUser);
+    })
+    .catch((err) => {
+      return Promise.reject(err);
+    });
+}
+
+export function unfavSavings(uid, savingsId){
+  return userModel.findOneAndUpdate(
+    {'_id': uid, 'savings._id': savingsId},
+    {'$set': {'savings.$.favorite': false}},
+    {'new': true})
+    .then((updatedUser) => {
+      if (updatedUser == null)
+        return Promise.reject('UserError: User or savings not found');
+
+      return Promise.resolve(updatedUser);
+    })
+    .catch((err) => {
+      return Promise.reject(err);
+    });
+}
